ca6dffc5a1b84bae9786dd5a1e56c535078968d3
Şermin KUŞ <149421629+berfinkus@users.noreply.github.com>
2026-02-10 01:51:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-/* TEMA DEGISTIRME */
+/* TEMA */
 const themes = [
-  {
-    bg: "linear-gradient(120deg, #0f172a, #020617)",
-    accent: "#f97316"
-  },
-  {
-    bg: "linear-gradient(120deg, #1e293b, #0f172a)",
-    accent: "#2563eb"
-  }
+  { bg: "linear-gradient(120deg,#0f172a,#020617)", accent: "#f97316" },
+  { bg: "linear-gradient(120deg,#1e293b,#0f172a)", accent: "#2563eb" }
 ];
 
-let currentTheme = 0;
-
+let t = 0;
 function changeTheme() {
-  currentTheme = (currentTheme + 1) % themes.length;
-  document.body.style.background = themes[currentTheme].bg;
-  document.documentElement.style.setProperty("--accent", themes[currentTheme].accent);
+  t = (t + 1) % themes.length;
+  document.body.style.background = themes[t].bg;
+  document.documentElement.style.setProperty("--accent", themes[t].accent);
 }
 
-/* PROFIL FOTO DONME */
-const images = document.querySelectorAll(".profile-img");
-let currentImage = 0;
+/* PROFIL SLIDER */
+const imgs = document.querySelectorAll(".profile-img");
+let i = 0;
 
 setInterval(() => {
-  images[currentImage].classList.remove("active");
-  currentImage = (currentImage + 1) % images.length;
-  images[currentImage].classList.add("active");
+  imgs[i].classList.remove("active");
+  i = (i + 1) % imgs.length;
+  imgs[i].classList.add("active");
 }, 3000);

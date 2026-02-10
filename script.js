6999e819da14313cfb092f51aa0ad2dcd785a476
Şermin KUŞ <149421629+berfinkus@users.noreply.github.com>
2026-02-10 23:20:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.querySelectorAll(".skill").forEach(skill => {
     const percent = skill.dataset.percent;
     const circle = skill.querySelector(".circle");
-    circle.style.background = `
-        conic-gradient(var(--accent) ${percent * 3.6}deg, #e5e7eb 0deg)
-    `;
+    circle.style.background =
+        `conic-gradient(var(--accent) ${percent * 3.6}deg, #e5e7eb 0deg)`;
     circle.innerText = percent + "%";
 });
 

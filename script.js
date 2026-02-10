1373f23e1b0e2ddb73c17fe65839c189c5b33474
Şermin KUŞ <149421629+berfinkus@users.noreply.github.com>
2026-02-10 12:54:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const imgs = document.querySelectorAll(".profile-img");
 let i = 0;
 
-setInterval(()=>{
-    imgs[i].classList.remove("active");
-    i = (i+1) % imgs.length;
-    imgs[i].classList.add("active");
-},3000);
+setInterval(() => {
+  imgs[i].classList.remove("active");
+  i = (i + 1) % imgs.length;
+  imgs[i].classList.add("active");
+}, 3000);
 
 const themes = [
- { bg:"linear-gradient(120deg,#1f2933,#111827)", accent:"#9a3412" },
- { bg:"linear-gradient(120deg,#0f172a,#020617)", accent:"#3b82f6" }
+  { bg:"linear-gradient(120deg,#1f2933,#111827)", accent:"#9a3412" },
+  { bg:"linear-gradient(120deg,#0f172a,#020617)", accent:"#3b82f6" }
 ];
 
 let t = 0;
 
 function changeTheme(){
- t = (t+1) % themes.length;
- document.body.style.background = themes[t].bg;
- document.documentElement.style.setProperty("--accent",themes[t].accent);
+  t = (t + 1) % themes.length;
+  document.body.style.background = themes[t].bg;
+  document.documentElement.style.setProperty("--accent", themes[t].accent);
 }
